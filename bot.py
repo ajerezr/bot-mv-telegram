00870ba80872a1cc6b94862b763eb18304a19b1f
ajerezr <ajerezr@github.io>
2016-01-06 19:57:05 +0100

    youtube module

--- bot.py
+++ bot.py
@@ -4,9 +4,13 @@ import telebot
 from telebot import types
 import time
 import config
+import urllib
+import json
+
 
 bot = telebot.TeleBot(config.token())
-administrador = token.admin()
+administrador = config.admin()
+ytoken = config.tokenYoutube()
 
 usuarios = [line.rstrip('\n') for line in open('files/usuarios')]
 
@@ -39,6 +43,39 @@ def command_start(m):
         aux.write( str(cid) + "\n")
         aux.close()
         bot.send_message( cid, "Bienvenido!")
+
+
+##############################
+# mensajes difundidos
+@bot.message_handler(commands=['all'])
+def command_all(m):
+    cid = m.chat.id
+    if cid != administrador:
+        bot.send_message( administrador, "El usuario con ID: " + str(cid) + " ha intentado utilizar el comando para enviar difundidos")
+    else:
+        for ID in usuarios:
+            try:
+                bot.send_message( int(ID), m.text[4:])
+            except:
+                bot.send_message( administrador, "Error enviando mensaje a: " + ID)
+            else:
+                bot.send_message( administrador, "Éxito enviando mensaje a: " + ID)
+
+###############
+# youtube
+
+@bot.message_handler(commands=['youtube'])
+def command_youtube(m):
+    cid = m.chat.id
+    query = m.text[4:]
+    # sustituir CLAVE API por tu API
+    link = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&q=%s&key={"+ytoken+"}" % query)
+    data = json.loads(link.read())
+    """Coge un video aleatorio del primero al tercero,
+    este rango se puede modificar o incluso dejar fijo"""
+    rnd_no = random.randrange(0,3)
+    id = data['items'][rnd_no]['id']['videoId']
+    bot.send_message(cid, "http://www.youtube.com/watch?v=" + id)
 
 #############################################
 # text
