1cb316e6989e465e61b1c97209923ea60b36b949
ajerezr <ajerezr@github.io>
2016-01-06 20:57:18 +0100

    en fin...

--- bot.py
+++ bot.py
@@ -4,78 +4,26 @@ import telebot
 from telebot import types
 import time
 import config
-import urllib
-import json
-
 
 bot = telebot.TeleBot(config.token())
-administrador = config.admin()
-ytoken = config.tokenYoutube()
 
-usuarios = [line.rstrip('\n') for line in open('files/usuarios')]
-
+#############################################
 # log
 
-def listener(messages):
-    for m in messages:
-        cid = m.chat.id
-        if cid > 0:
-            mensaje = str(m.chat.first_name) + " [" + str(cid) + "]: " + m.text
-        else:
-            mensaje = str(m.from_user.first_name) + "[" + str(cid) + "]: " + m.text
-        f = open( 'files/log', 'a')
-        f.write(mensaje + "\n")
-        f.close()
-        print mensaje
+# def listener(messages):
+#     for m in messages:
+#         cid = m.chat.id
+#         if cid > 0:
+#             mensaje = str(m.chat.first_name) + " [" + str(cid) + "]: " + m.text
+#         else:
+#             mensaje = str(m.from_user.first_name) + "[" + str(cid) + "]: " + m.text
+#         f = open( 'files/log', 'a')
+#         f.write(mensaje + "\n")
+#         f.close()
+#         print(mensaje)
 
 
-bot.set_update_listener(listener)
-
-######################
-# Control de usuarios
-
-@bot.message_handler(commands=['start'])
-def command_start(m):
-    cid = m.chat.id
-    if not str(cid) in usuarios:
-        usuarios.append(str(cid))
-        aux = open( 'files/usuarios', 'a')
-        aux.write( str(cid) + "\n")
-        aux.close()
-        bot.send_message( cid, "Bienvenido!")
-
-
-##############################
-# mensajes difundidos
-@bot.message_handler(commands=['all'])
-def command_all(m):
-    cid = m.chat.id
-    if cid != administrador:
-        bot.send_message( administrador, "El usuario con ID: " + str(cid) + " ha intentado utilizar el comando para enviar difundidos")
-    else:
-        for ID in usuarios:
-            try:
-                bot.send_message( int(ID), m.text[4:])
-            except:
-                bot.send_message( administrador, "Error enviando mensaje a: " + ID)
-            else:
-                bot.send_message( administrador, "Éxito enviando mensaje a: " + ID)
-
-###############
-# youtube
-
-@bot.message_handler(commands=['youtube'])
-def command_youtube(m):
-    cid = m.chat.id
-    query = m.text[4:]
-    # sustituir CLAVE API por tu API
-    link = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&q=%s&key={"+ytoken+"}" % query)
-    data = json.loads(link.read())
-    """Coge un video aleatorio del primero al tercero,
-    este rango se puede modificar o incluso dejar fijo"""
-    rnd_no = random.randrange(0,3)
-    id = data['items'][rnd_no]['id']['videoId']
-    bot.send_message(cid, "http://www.youtube.com/watch?v=" + id)
+# bot.set_update_listener(listener)
 
 #############################################
 # text
