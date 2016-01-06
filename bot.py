dfbab5c904fb3a48325aeb13f46170a445cb7322
ajerezr <ajerezr@github.io>
2016-01-06 18:07:25 +0000

    users

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import time
 import token
 
 bot = telebot.TeleBot(token.t())
+usuarios = [line.rstrip('\n') for line in open('files/usuarios')]
 
 # log
 
@@ -16,7 +17,7 @@ def listener(messages):
             mensaje = str(m.chat.first_name) + " [" + str(cid) + "]: " + m.text
         else:
             mensaje = str(m.from_user.first_name) + "[" + str(cid) + "]: " + m.text 
-        f = open( 'log/log', 'a') 
+        f = open( 'files/log', 'a') 
         f.write(mensaje + "\n")
         f.close()
         print mensaje
@@ -24,17 +25,25 @@ def listener(messages):
 
 bot.set_update_listener(listener)
 
+######################
+# Control de usuarios
+
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    if not str(cid) in usuarios:
+        usuarios.append(str(cid))
+        aux = open( 'files/usuarios', 'a')
+        aux.write( str(cid) + "\n")
+        aux.close()
+        bot.send_message( cid, "Bienvenido!")
+
 #############################################
 # text
 @bot.message_handler(commands=['windows'])
 def command_windows(m):
     cid = m.chat.id
     bot.send_message( cid, 'Vete a la mierda')
-
-@bot.message_handler(commands=['prueba'])
-def command_prueba(m):
-    cid = m.chat.id
-    bot.send_message( cid, 'log work')
 
 @bot.message_handler(commands=['hilo'])
 def command_hilo(m):
