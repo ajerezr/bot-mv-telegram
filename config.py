d4603ab7d3bb7f5a3fb3b4c87fbcccef9fcd3356
Alex <awicket@users.noreply.github.com>
2016-05-01 20:15:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # Set your current API Token here. Obtain your token by creating a bot in: telegram.me/BotFather
-TOKEN = 'None'
+TOKEN = None
 
 # Set your current Api key from api.openwheatermap for run wheater module
 # Insert key or None for disable this module
-OpenWheatKey = 'None'
+OpenWheatKey = None
 
 def getToken():
     return TOKEN
