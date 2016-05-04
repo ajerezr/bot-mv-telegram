70fab561a17232a84781a8758f2c68c8069796d6
esCFF <kaesarrs@gmail.com>
2016-05-04 20:28:33 +0000

    show bot mem usage

--- modules/uptime.py
+++ modules/uptime.py
@@ -3,6 +3,15 @@ import platform
 import uptime
 from datetime import timedelta
 import time
+import subprocess
+import os
+
+def memory_usage_ps():
+    import subprocess
+    out = subprocess.Popen(['ps', 'v', '-p', str(os.getpid())],stdout=subprocess.PIPE).communicate()[0].split(b'\n')
+    vsz_index = out[0].split().index(b'RSS')
+    mem = float(out[1].split()[vsz_index]) / 1024
+    return "{0:.2f}".format(mem)
 
 def uptime_string(startup_time_in_seconds):
 	#Machine info
@@ -14,8 +23,12 @@ def uptime_string(startup_time_in_seconds):
 	now = time.time()
 	delta = now-startup_time_in_seconds
 	bot_uptime = str(timedelta(seconds=int(delta)))
-	
-	string="\U0001F4BB Running on "+uname[0]+" "+uname[2]+" "+uname[4]+"\n\U0000231B Machine Uptime: "+uptime_string+"\n\U0001F916 Bot uptime: "+bot_uptime
-	
+	#Get memory usage with ps
+	memory = memory_usage_ps()
+	#Make messsge
+	string=""
+	string+="\U0001F4BB Running on "+uname[0]+" "+uname[2]+" "+uname[4]+"\n"
+	string+="\U0000231B Machine Uptime: "+uptime_string+"\n"
+	string+="\U0001F916 Bot uptime: "+bot_uptime+"\n"
+	string+="\U0001F4CA Bot mem used: "+memory+"Mb"
 	return string
-	
